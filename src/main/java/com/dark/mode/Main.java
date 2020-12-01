d659c86cb43d047de904b6742b4857e5090df191
DarkMode : On <muffintoppings36@gmail.com>
2020-12-01 20:48:20 +0800

    update gitignore file

--- src/main/java/com/dark/mode/Main.java
+++ src/main/java/com/dark/mode/Main.java
@@ -4,7 +4,7 @@ import main.java.com.dark.mode.app.Number;
 
 public class Main {
     public static void main(String[] args) {
-        long num = 912_231_532_423_123L;
+        long num = 912_231_532_123_123L;
         Number number = new Number();
         System.out.printf("%s -> %s \n", num, number.toWords(num));
     }
