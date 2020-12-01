31edb23381f9410427fb938ace01cf2681fd0c91
DarkMode : On <muffintoppings36@gmail.com>
2020-12-01 20:20:20 +0800

    separate utility methods

---
+++ src/main/java/com/dark/mode/utility/Utility.java
@@ -0,0 +1,10 @@
+package main.java.com.dark.mode.utility;
+
+public final class Utility {
+    private Utility() {
+    }
+
+    public static String separator(String s) {
+        return s.isEmpty() ? "" : ",";
+    }
+}
